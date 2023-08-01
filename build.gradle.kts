b92ab293038ebe1161837db860710526829ec94a
anasoid <oufdou.anas@gmail.com>
2023-08-01 18:13:40 +0100

    add sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 
 version = "0.0.1-SNAPSHOT"
 
+
+plugins {
+    id("org.sonarqube") version "4.2.1.3168"
+}
 allprojects {
     group = "org.anasoid.jmeter.cloud"
     version = rootProject.version
