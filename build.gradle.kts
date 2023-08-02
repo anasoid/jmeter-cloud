5f9f77e9e0f7028cbf283c0332128de90b4ec4f9
anasoid <oufdou.anas@gmail.com>
2023-08-02 07:24:52 +0100

    cc codcov/sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,16 @@ version = "0.0.1-SNAPSHOT"
 plugins {
     id("org.sonarqube") version "4.2.1.3168"
 }
+
+sonar {
+    properties {
+        property("sonar.projectKey"), "anasoid_jmeter-cloud"
+        property("sonar.organization"), "anasoid"
+        property("sonar.host.url"), "https://sonarcloud.io"
+        property("sonar.exclusions"), "**/generated/**"
+        property("sonar.coverage.jacoco.xmlReportPaths"), "${rootProject.path}/buildUtils/code-coverage-report/build/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml"
+    }
+}
 allprojects {
     group = "org.anasoid.jmeter.cloud"
     version = rootProject.version
